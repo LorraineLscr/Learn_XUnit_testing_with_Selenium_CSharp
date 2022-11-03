7499aba4eb0abea65d0ba6aad76f7195951c362a
LorraineLscr <lorraiine76@hotmail.fr>
2022-11-03 12:25:23 +0100

    🚧 Work in progress : Contexte de class

---
+++ SeleniumTestProject/firstFixture/DriverFixture.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestProject.firstFixture
+
+{
+    public class DriverFixture : IDisposable
+    {
+        public DriverFixture(WebDriverWait webDriverWait, IWebDriver driver)
+        {
+            WebDriverWait = webDriverWait;
+            Driver = driver;
+        }
+
+        public WebDriverWait WebDriverWait { get; set; }   
+        public IWebDriver Driver { get; set; }   
+    }
+}
